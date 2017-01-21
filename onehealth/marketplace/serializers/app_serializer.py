cbece19f3a0740855f08a30a9aa20e3b01c1d80c
patlola <praveen.patlola@practo.com>
2017-01-21 14:56:07 +0530

    combining files and sending the resposne

--- onehealth/marketplace/serializers/app_serializer.py
+++ onehealth/marketplace/serializers/app_serializer.py
@@ -9,6 +9,7 @@ class AppsSerializer(serializers.ModelSerializer):
 
     install_url = serializers.SerializerMethodField()
     image_url = serializers.SerializerMethodField()
+    build_path = serializers.SerializerMethodField()
 
     def get_install_url(self, obj):
         """a function to return the change in field name."""
@@ -17,6 +18,9 @@ class AppsSerializer(serializers.ModelSerializer):
     def get_image_url(self, obj):
         return settings.HOSTED_URL + obj.logo.url
 
+    def get_build_path(self, obj):
+        return obj.build.path
+
     class Meta:
         model = Apps
-        fields = ('id', 'name', 'install_url', 'image_url')
+        fields = ('id', 'name', 'install_url', 'image_url', 'build_path')
