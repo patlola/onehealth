82f4800e5198236461eb0e00af17475766fe6c33
patlola <praveen.patlola@practo.com>
2017-01-21 16:24:54 +0530

    changed name

--- onehealth/marketplace/admin.py
+++ onehealth/marketplace/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Apps
 
-admin.site.site_title = 'One Health Apps'
-admin.site.site_header = 'One Health Apps'
+admin.site.site_title = 'Practo - One Health App'
+admin.site.site_header = 'Practo - One Health App'
 
 
 @admin.register(Apps)
