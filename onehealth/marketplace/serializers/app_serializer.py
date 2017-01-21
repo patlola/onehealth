3924de1c5b47f7ba3bd21d81af95e5dec9a7ae81
patlola <praveen.patlola@practo.com>
2017-01-21 13:44:09 +0530

    image url update

--- onehealth/marketplace/serializers/app_serializer.py
+++ onehealth/marketplace/serializers/app_serializer.py
@@ -8,15 +8,15 @@ class AppsSerializer(serializers.ModelSerializer):
     """city serializers class for serializing the data for city."""
 
     install_url = serializers.SerializerMethodField()
-    logo_url = serializers.SerializerMethodField()
+    image_url = serializers.SerializerMethodField()
 
     def get_install_url(self, obj):
         """a function to return the change in field name."""
         return settings.HOSTED_URL + obj.build.url
 
-    def get_logo_url(self, obj):
+    def get_image_url(self, obj):
         return settings.HOSTED_URL + obj.logo.url
 
     class Meta:
         model = Apps
-        fields = ('id', 'name', 'install_url', 'logo_url')
+        fields = ('id', 'name', 'install_url', 'image_url')
